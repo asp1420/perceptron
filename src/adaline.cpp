c69ae389cd71ca43291d57b37d10192fb3d41d74
asp1420 <ab.sanchezperez@gmail.com>
2018-01-11 07:45:25 -0600

    Update adaline.cpp

--- src/adaline.cpp
+++ src/adaline.cpp
@@ -1,45 +1,64 @@
 #include "adaline.h"
 
-Adaline::Adaline(arma::mat X, arma::mat t)
+Adaline::Adaline(mat X, mat t) : SupervisedNetwork(X, t)
 {
-    this->X = X;
-    this->t = t;
-    arma::arma_rng::set_seed_random();
-    W = arma::randu(t.n_cols,X.n_cols);
-    b = 1;
+    arma_rng::set_seed_random();
+    W = randu(t.n_cols,X.n_cols);
+    b = 0.1;
+    cout <<"Weights initialization:" <<endl;
+    weights();
 }
 
 void Adaline::train(void)
 {
+    unsigned int epoch = 0;
+    vec E;
     double error = 0.0;
-    arma::vec e_va;
 
     do
     {
-        e = arma::zeros(t.n_rows);
+        E = zeros(t.n_rows);
         for(unsigned int i = 0 ; i < X.n_rows ; i++)
         {
-            arma::mat o = Activation::sigmoid(X.row(i) * W.t() + b);
-            arma::mat y = Activation::dSigmoid(o);
-            arma::mat e = t(i) - o;
-            W = W+(X.row(i)*eta*e(0)*y(0));
-            b = b+(eta*e(0)*y);
-            e_va(i) = pow(e(0), 2);
+            mat v = Activation::sigmoid(X.row(i) * W.t() + b);
+            mat y = Activation::dSigmoid(v);
+            mat e = t(i) - v;
+            W = W + (e * y * eta * X.row(i));
+            b = b + (e * y * eta);
+            E(i) = as_scalar(e * e);
         }
-        error = arma::sum(e_va);
-        std::cout <<"=" <<error <<std::endl;
-    }while(error > minError);
+        error = sum(E) / X.n_rows;
+        cout <<"error=" <<error <<endl;
+        epoch++;
+    }while(error > minError && epoch < maxEpochs);
+
+    cout <<"training statistics:" <<endl;
+    cout <<"- Epoch: " <<epoch <<endl;
+    cout <<"- Error: " <<error <<endl;
+    cout <<endl;
+}
+
+void Adaline::weights()
+{
+    cout <<"W=" <<W <<"b=" <<b <<endl;
 }
 
 void Adaline::simulate(const double noise)
 {
-    arma::mat T = X + noise;
-    arma::mat y = Activation::sigmoid(T * W.t() + b(0));
-    std::cout <<"Y=" <<std::endl <<y <<std::endl;
+    cout <<"Training set (X):" <<endl;
+    cout <<X <<endl;
+    cout <<"Target set (t):" <<endl;
+    cout <<t <<endl;
+    cout <<"Weights after training:" <<endl;
+    weights();
+    mat T = X + noise;
+    mat y = Activation::sigmoid(T * W.t() + as_scalar(b));
+    cout <<"Output:" <<endl;
+    cout <<y <<endl;
 }
 
-void Adaline::simulate(const arma::mat X)
+void Adaline::simulate(const mat X)
 {
-    arma::mat y = Activation::step(X * W.t() + b(0));
-    std::cout <<"Y=" <<std::endl <<y <<std::endl;
+    mat y = Activation::step(X * W.t() + as_scalar(b));
+    cout <<"Y=" <<endl <<y <<endl;
 }
